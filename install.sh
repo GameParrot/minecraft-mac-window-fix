6bb1c6c70cce8aaa84d596e48c7939b5317fb6ed
GameParrot <85067619+GameParrot@users.noreply.github.com>
2021-08-14 17:00:01 -0400

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+unzip ./mac-runtime-patch.zip -d ~/Library/Application\ Support/minecraft/
+echo 'Successfully installed. To complete installation, set the Java executable to "'"$HOME"'/Library/Application Support/minecraft/mac-runtime-patch/java-runtime-alpha/mclaunch" for Java 16 or "'"$HOME"'/Library/Application Support/minecraft/mac-runtime-patch/jre-legacy/mclaunch" for Java 8.'
