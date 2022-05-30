dfc55b3b0fd09fdf578393dd1a4640c0b92c15d1
GameParrot <85067619+GameParrot@users.noreply.github.com>
2022-05-30 17:59:34 -0400

    Delete install.sh

--- install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-unzip -o ./mac-runtime-patch.zip -d ~/Library/Application\ Support/minecraft/
-echo 'Successfully installed. To complete installation, set the Java executable to "'"$HOME"'/Library/Application Support/minecraft/mac-runtime-patch/java-runtime-beta/mclaunch" for Java 17, "'"$HOME"'/Library/Application Support/minecraft/mac-runtime-patch/java-runtime-alpha/mclaunch" for Java 16, or "'"$HOME"'/Library/Application Support/minecraft/mac-runtime-patch/jre-legacy/mclaunch" for Java 8.'
